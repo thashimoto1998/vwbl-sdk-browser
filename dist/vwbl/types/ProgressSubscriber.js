9120b1d72163fa988a32fecfdc7ddad9c6e5e590
Tatsuki <40402518+thashimoto1998@users.noreply.github.com>
2023-01-16 13:38:44 +0900

    Delete dist directory

--- dist/vwbl/types/ProgressSubscriber.js
+++
@@ -1,9 +0,0 @@
-export var StepStatus;
-(function (StepStatus) {
-    StepStatus["MINT_TOKEN"] = "MINT_TOKEN";
-    StepStatus["CREATE_KEY"] = "CREATE_KEY";
-    StepStatus["ENCRYPT_DATA"] = "ENCRYPT_DATA";
-    StepStatus["UPLOAD_CONTENT"] = "UPLOAD_CONTENT";
-    StepStatus["UPLOAD_METADATA"] = "UPLOAD_METADATA";
-    StepStatus["SET_KEY"] = "SET_KEY";
-})(StepStatus || (StepStatus = {}));
--- dist/vwbl/types/UploadMetadataType.js
+++
@@ -1,6 +0,0 @@
-export var UploadMetadataType;
-(function (UploadMetadataType) {
-    UploadMetadataType[UploadMetadataType["S3"] = 0] = "S3";
-    UploadMetadataType[UploadMetadataType["IPFS"] = 1] = "IPFS";
-    UploadMetadataType[UploadMetadataType["CUSTOM"] = 2] = "CUSTOM";
-})(UploadMetadataType || (UploadMetadataType = {}));
